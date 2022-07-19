1804ca6e234f91844a7b21ddecf6f9667a69791f
yuirin83 <yuirin83@gmail.com>
2022-07-20 01:41:11 +0900

    0720-0141

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,6 +3,9 @@ let laySpeed = 0
 let bgSpeed_num = 0.0003
 let laySpeed_num = 0.002
 
+let beforeY
+let correntY
+
 function updateScroll(event) {
   const image = document.querySelector('.scroll-image')
   const layer = document.querySelector('.layer')
@@ -17,32 +20,45 @@ window.addEventListener('scroll', function () {
   scrollNum.textContent = window.pageYOffset
   let scroll = window.pageYOffset
 
-  if (scroll > 70000) {
-    // if (bgSpeed > 0 && laySpeed > 0) {
-    //   bgSpeed -= bgSpeed_num
-    //   laySpeed -= laySpeed_num
-    // }
-  } else if (scroll > 30000) {
-    let isTop = Boolean(false)
-    let bgSpeed_max
-    let laySpeed_max
-    if (!isTop) {
-      bgSpeed_max = bgSpeed
-      laySpeed_max = laySpeed
-      bgSpeed = bgSpeed_max
-      laySpeed = laySpeed_max
-      isTop = Boolean(true)
+  if (beforeY < scroll) {
+    // if (scroll > 70000) {
+    //   // if (bgSpeed > 0 && laySpeed > 0) {
+    //   //   bgSpeed -= bgSpeed_num
+    //   //   laySpeed -= laySpeed_num
+    //   // }
+    // } else if (scroll > 30000) {
+    //   let isTop = Boolean(false)
+    //   let bgSpeed_max
+    //   let laySpeed_max
+    //   if (!isTop) {
+    //     bgSpeed_max = bgSpeed
+    //     laySpeed_max = laySpeed
+    //     bgSpeed = bgSpeed_max
+    //     laySpeed = laySpeed_max
+    //     isTop = Boolean(true)
+    //   } else {
+    //     bgSpeed = bgSpeed_max
+    //     laySpeed = laySpeed_max
+    //   }
+    // } else
+    if (scroll > 5300) {
+      bgSpeed += bgSpeed_num
+      laySpeed += laySpeed_num
     } else {
-      bgSpeed = bgSpeed_max
-      laySpeed = laySpeed_max
+      bgSpeed = 0
+      laySpeed = 0
     }
-  } else if (scroll > 5300) {
-    bgSpeed += bgSpeed_num
-    laySpeed += laySpeed_num
-  } else {
-    bgSpeed = 0
-    laySpeed = 0
   }
 
-  console.log(bgSpeed)
+  beforeY = window.pageYOffset
+  // console.log(bgSpeed)
+
+  var targetElement = document.getElementById('tonnel-entrance')
+  var clientRect = targetElement.getBoundingClientRect()
+  var x = clientRect.left
+  console.log(x)
+
+  if(x <= 350){
+    
+  }
 })
