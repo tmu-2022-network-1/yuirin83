f10934d435a09113be74fd2b1001d1053b906996
yuirin83 <yuirin83@gmail.com>
2022-07-20 18:47:56 +0900

    luxy

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,64 +1,90 @@
 let bgSpeed = 0
 let laySpeed = 0
-let bgSpeed_num = 0.0003
-let laySpeed_num = 0.002
 
 let beforeY
 let correntY
+const image = document.querySelector('.scroll-image')
+const layer = document.querySelector('.layer')
 
-function updateScroll(event) {
-  const image = document.querySelector('.scroll-image')
-  const layer = document.querySelector('.layer')
-  image.scrollLeft = document.documentElement.scrollTop * bgSpeed
-  layer.scrollLeft = document.documentElement.scrollTop * laySpeed
-}
-document.body.onscroll = updateScroll
+// function updateScroll(event) {
+//   image.scrollLeft = document.documentElement.scrollTop * bgSpeed
+//   layer.scrollLeft = document.documentElement.scrollTop * laySpeed
+// }
+// document.body.onscroll = updateScroll
 
 const scrollNum = document.getElementById('scroll-num')
 
-window.addEventListener('scroll', function () {
-  scrollNum.textContent = window.pageYOffset
-  let scroll = window.pageYOffset
+// window.addEventListener('scroll', function () {
+//   scrollNum.textContent = window.scrollY
+//   let scroll = window.scrollY
 
-  if (beforeY < scroll) {
-    // if (scroll > 70000) {
-    //   // if (bgSpeed > 0 && laySpeed > 0) {
-    //   //   bgSpeed -= bgSpeed_num
-    //   //   laySpeed -= laySpeed_num
-    //   // }
-    // } else if (scroll > 30000) {
-    //   let isTop = Boolean(false)
-    //   let bgSpeed_max
-    //   let laySpeed_max
-    //   if (!isTop) {
-    //     bgSpeed_max = bgSpeed
-    //     laySpeed_max = laySpeed
-    //     bgSpeed = bgSpeed_max
-    //     laySpeed = laySpeed_max
-    //     isTop = Boolean(true)
-    //   } else {
-    //     bgSpeed = bgSpeed_max
-    //     laySpeed = laySpeed_max
-    //   }
-    // } else
-    if (scroll > 5300) {
-      bgSpeed += bgSpeed_num
-      laySpeed += laySpeed_num
-    } else {
-      bgSpeed = 0
-      laySpeed = 0
-    }
+//   if (beforeY < scroll) {
+//     // if (scroll > 70000) {
+//     //   // if (bgSpeed > 0 && laySpeed > 0) {
+//     //   //   bgSpeed -= bgSpeed_num
+//     //   //   laySpeed -= laySpeed_num
+//     //   // }
+//     // } else if (scroll > 30000) {
+//     //   let isTop = Boolean(false)
+//     //   let bgSpeed_max
+//     //   let laySpeed_max
+//     //   if (!isTop) {
+//     //     bgSpeed_max = bgSpeed
+//     //     laySpeed_max = laySpeed
+//     //     bgSpeed = bgSpeed_max
+//     //     laySpeed = laySpeed_max
+//     //     isTop = Boolean(true)
+//     //   } else {
+//     //     bgSpeed = bgSpeed_max
+//     //     laySpeed = laySpeed_max
+//     //   }
+//     // } else
+//     if (scroll > 5300) {
+//       bgSpeed += bgSpeed_num
+//       laySpeed += laySpeed_num
+//     } else {
+//       bgSpeed = 0
+//       laySpeed = 0
+//     }
+//   }
+
+//   beforeY = window.pageYOffset
+//   // console.log(bgSpeed)
+
+//   var targetElement = document.getElementById('tonnel-entrance')
+//   var clientRect = targetElement.getBoundingClientRect()
+//   var x = clientRect.left
+
+//   if (x <= 350) {
+//   }
+// })
+
+let luxyScroll = 0;
+const elem = document.getElementById('luxy')
+
+var observer = new MutationObserver(function () {
+  const style = window.getComputedStyle(elem)
+  const luxyCheck = style.getPropertyValue('transform')
+  luxyScroll = luxyCheck.slice(23, -1)
+  if(!luxyScroll){
+    luxyScroll = 0
   }
+  
+  console.log(luxyScroll)
+  image.scrollLeft = luxyScroll * (1 + bgSpeed);
+  layer.scrollLeft = luxyScroll * (1 + laySpeed);
+  scrollNum.textContent = luxyScroll;
 
-  beforeY = window.pageYOffset
-  // console.log(bgSpeed)
-
-  var targetElement = document.getElementById('tonnel-entrance')
-  var clientRect = targetElement.getBoundingClientRect()
-  var x = clientRect.left
-  console.log(x)
-
-  if(x <= 350){
-    
+  if(luxyScroll > 5300){
+    bgSpeed += 0.001
+    laySpeed += 0.0015
+  }else{
+    bgSpeed = -1
+    laySpeed = -1
   }
 })
+
+const config = {
+  attributes: true,
+}
+observer.observe(elem, config)
